c9345a9aa356f3df2928fca2261eb0cbccd20e46
Sangmin Lee <lsm3861@gmail.com>
2022-04-20 00:37:13 +0900

    Complete QuotesGenerator

---
+++ QuitesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuitesGenerator
+//
+//  Created by Sangmin Lee on 2022/04/20.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
